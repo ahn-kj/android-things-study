681de07dc099d754854361272dad449f64140abc
akjhh <rnlwjd0613@gmail.com>
2017-04-24 16:48:08 +0900

    LED 파이어베이스 예제 추가
    Readme 수정

--- LedWithFirebase/app/src/main/java/singularity/io/iosingularityledwithfirebase/MainActivity.java
+++ LedWithFirebase/app/src/main/java/singularity/io/iosingularityledwithfirebase/MainActivity.java
@@ -1,13 +1,105 @@
 package singularity.io.iosingularityledwithfirebase;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int INTERVAL_BETWEEN_BLINKS_MS = 1000;
+    private static final String LED = "BCM6";
+
+    private Handler mHandler = new Handler();
+    private Gpio mLedGpio;
+
+
+
+    private Runnable mBlinkRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mLedGpio == null) {
+                return;
+            }
+            try {
+                // Toggle the GPIO state
+                mLedGpio.setValue(!mLedGpio.getValue());
+                Log.d(TAG, "State set to " + mLedGpio.getValue());
+                mHandler.postDelayed(mBlinkRunnable, INTERVAL_BETWEEN_BLINKS_MS);
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        Log.d(TAG, "Available GPIO: " + service.getGpioList());
+        try {
+            mLedGpio = service.openGpio(LED);
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+            Log.i(TAG, "Start blinking LED GPIO pin");
+//            mHandler.post(mBlinkRunnable);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+                try {
+                    if("ON".equals(value)){
+                        mLedGpio.setValue(true);
+                    }else{
+                        mLedGpio.setValue(false);
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacks(mBlinkRunnable);
+        Log.i(TAG, "Closing LED GPIO pin");
+        try {
+            mLedGpio.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        } finally {
+            mLedGpio = null;
+        }
     }
 }
--- README.md
+++ README.md
@@ -7,12 +7,30 @@ Android Things 를 이용하여 IOT 를 스터디하는 프로젝트입니다.
 ---
 
 ## 프로젝트 목록
-|프로젝트 이름|프로젝트 설명|링크|
-|:--:|:--|:--|
+|프로젝트 이름|프로젝트 설명|링크|유튜브링크|
+|:--:|:--|:--|:--|
 |HelloThings|LED 1개를 1초마다 ON/OFF 를 시키는 매우 간단한 프로젝트|[https://github.com/ahn-kj/android-things-study/tree/master/HelloThings](https://github.com/ahn-kj/android-things-study/tree/master/HelloThings)|
 |Button|Android Things 구글 레퍼런스에 등록되어 있는 예제를 구현한 프로젝트. 스위치를 연결해 스위치를 누르고 있는 동안에만 LED 가 켜지는 샘플|[https://github.com/ahn-kj/android-things-study/tree/master/Button](https://github.com/ahn-kj/android-things-study/tree/master/Button)|
 |LedWithFirebase|Firebase 를 연동하여 원격으로 LED 를 제어하는 프로젝트||
 
+## 공통 준비물 
+
+#### 라즈베리파이 3
+
+Android Things 는 라즈베리파이 3 를 지원합니다.
+라즈베리파이3 이외에도 지원하는 디바이스는 다음과 같습니다.
+
+<iframe type="html" width="420" height="315" src="https://developer.android.com/things/hardware/developer-kits.html#hardware_platforms" frameborder="0"></iframe>
+
+[지원 디바이스 참조 링크](https://developer.android.com/things/hardware/developer-kits.html#hardware_platforms)
+
+#### MICRO SD CARD
+
+#### Ethenet cable
+
+#### Android SDK
+
+## 라즈베리파이3 에 Android Things 올리는 방법
 
 ## 각 프로젝트 실행 방법
 
