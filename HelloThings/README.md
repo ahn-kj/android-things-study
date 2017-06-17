c9d5b8828e149c996ebad244942b88a34b1dc08d
akjhh <rnlwjd0613@gmail.com>
2017-06-17 13:17:45 +0900

    * 리드미 파일 내용 추가 * 주석 추가

--- HelloThings/README.md
+++ HelloThings/README.md
@@ -1,5 +1,7 @@
 # 프로젝트 개요
 
+[![IMAGE ALT TEXT](http://img.youtube.com/vi/jvyNrBTJSbA&t/0.jpg)](http://www.youtube.com/watch?v=jvyNrBTJSbA&t "Video Title")
+
 Android Things 와 라즈베리파이 3 을 연계하여 LED 1개를 제어하는 매우 간단한 샘플입니다.
 
 이 프로젝트는 다음 블로그를 참조하여 작성되었습니다. 
@@ -28,7 +30,61 @@ Thanks to mplacona.
 
 프로젝트를 실제로 실행하기 위한 방법을 가이드합니다.
 
-## 코드 설명
 
-필요한 경우 코드 설명을 합니다.
+## Hello Things 코드 설명 및 동작 흐름.
+
+#### Android Things 프로젝트 설정
+
+* app/build.gradle 에 Android Things 라이브러리 의존성을 추가.
+
+```groovy
+dependencies {
+    // 안드로이드 팅스 라이브러리 디펜덴시. provided 컴파일시 클래스파일을 포함시키지 않음.
+    provided 'com.google.android.things:androidthings:0.2-devpreview'
+}
+```
+
+* AndroidManifest.xml 에 library 사용 추가
+
+```xml
+<uses-library android:name="com.google.android.things"/>
+```
+
+* IOT_LAUNCHER 인텐트필터 카테고리 추가
+
+```xml
+<category android:name="android.intent.category.IOT_LAUNCHER"/>
+```
+
+여기까지 설정하면 Android Things 프로젝트로 실행할 수 있게 된다.
+
+#### GPIO 를 이용한 LED ON/OFF 방법
+
+PeripheralManagerService 는 안드로이드 팅스에서 PinOut 을 관리할 수 있는 서비스이다. PeripheralManagerService 를 사용하여 
+
+* PeripheralManagerService 객체를 생성한다.
+
+```java
+PeripheralManagerService service = new PeripheralManagerService();
+```
+
+* "BCM6" 이름의 GPIO 를 오픈한다.
+
+```java
+mLedGpio = service.openGpio("BCM6");
+```
+
+* 입출력 설정을 체크한다.
+
+```java
+mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+```
+
+* GPIO 포트를 사용하여 LED ON/OFF
+
+```java
+mLedGpio.setValue(!mLedGpio.getValue());
+```
+
+
 
--- HelloThings/app/src/main/java/rocks/androidthings/hellothings/MainActivity.java
+++ HelloThings/app/src/main/java/rocks/androidthings/hellothings/MainActivity.java
@@ -13,13 +13,22 @@ import java.io.IOException;
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
-
     private static final int INTERVAL_BETWEEN_BLINKS_MS = 1000;
+
+    /**
+     * <pre>
+     * GPIO(General-purpose input/output) 의 이름. 라즈베리파이에서 GPIO 및 핀 아웃은 다음 이미지 참조.
+     * <img src="https://developer.android.com/things/images/pinout-raspberrypi.png" />
+     * </pre>
+     */
     private static final String LED = "BCM6";
 
     private Handler mHandler = new Handler();
     private Gpio mLedGpio;
 
+    /**
+     * 1초 마다 LED 를 ON/OFF 하는 Runnable
+     */
     private Runnable mBlinkRunnable = new Runnable() {
         @Override
         public void run() {
@@ -27,9 +36,11 @@ public class MainActivity extends Activity {
                 return;
             }
             try {
-                // Toggle the GPIO state
+                // LED GPIO 의 값을 현재 값의 반대로 설정
                 mLedGpio.setValue(!mLedGpio.getValue());
                 Log.d(TAG, "State set to " + mLedGpio.getValue());
+
+                // 타이머처럼 동작하기 위해 mBlinkRunnable 을 postDelayed
                 mHandler.postDelayed(mBlinkRunnable, INTERVAL_BETWEEN_BLINKS_MS);
             } catch (IOException e) {
                 Log.e(TAG, "Error on PeripheralIO API", e);
@@ -42,12 +53,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // PeripheralManagerService 는 주변장치 관리자 같은거
         PeripheralManagerService service = new PeripheralManagerService();
+
+        // gpio 현재 하드웨어에 존재하는 GPIO 리스트를 로깅
         Log.d(TAG, "Available GPIO: " + service.getGpioList());
+
         try {
+            // PeripheralManagerService 를 이용해 LED GPIO 를 오픈
             mLedGpio = service.openGpio(LED);
+            // 일단 입출력 설정을 체크한다.
+            // DIRECTION_OUT_INITIALLY_LOW 는 GPIO 에서 출력이 가능한가 체크하는것으로 추정.
             mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
             Log.i(TAG, "Start blinking LED GPIO pin");
+
+            // 핸들러로 1초 마다 LED 를 ON/OFF 하는 runnable post
             mHandler.post(mBlinkRunnable);
         } catch (IOException e) {
             Log.e(TAG, "Error on PeripheralIO API", e);
@@ -57,9 +77,12 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        // mBlinkRunnable 동작 멈춤
         mHandler.removeCallbacks(mBlinkRunnable);
         Log.i(TAG, "Closing LED GPIO pin");
+
         try {
+            // GPIO 닫음. 닫지 않는 경우 다른곳에서 열수 없음.
             mLedGpio.close();
         } catch (IOException e) {
             Log.e(TAG, "Error on PeripheralIO API", e);
